5ee4f8f5d9120d331908a6d6c762211acc425fcf
pratik khond <112702992+pratik1720@users.noreply.github.com>
2023-11-23 00:11:49 +0530

    Adding project files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved </p>
+    </div>
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://youtube.com/6packprogrammer" target={'blank'}>YouTube</a>
+            <a href="https://instagram.com/pratikkhond" target={'blank'}>Instagram</a>
+            <a href="https://github.com/pratik1720" target={'blank'}>Github</a>
+        </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
